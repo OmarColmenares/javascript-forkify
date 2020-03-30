4a17405177d02a474e91cb59795a5f9bf530b3e1
Omar <omarnazaret@gmail.com>
2020-03-30 09:39:00 -0400

    search controller, and searchView

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,33 @@
-// Global app controller
+import Search from './models/Search';
+//import * as searchView from './views/searchView'
+import * as searchView from './views/searchView'
+import {elements, renderLoader, clearLoader} from './views/base'
+
+const state =  {}
+
+const controlSearch = async () => {
+    // 1) Get query from view
+    const query = searchView.getInput();
+    console.log(query);
+    if(query){
+        // 2) New search object and add to state
+        state.search = new Search(query)
+
+        // 3) Prepare UI for results 
+        searchView.clearInput();
+        searchView.clearResults();
+        renderLoader(elements.searchRes);
+
+        // 4) Search for recipes
+        await state.search.getResults()
+        
+        // 5) Render results on UI
+        clearLoader();
+        searchView.renderResults(state.search.result);
+    }
+}
+
+elements.searchForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    controlSearch();
+});
---
+++ src/js/views/base.js
@@ -0,0 +1,27 @@
+export const elements = {
+    searchForm: document.querySelector('.search'),
+    searchInput: document.querySelector('.search__field'),
+    resultsList: document.querySelector('.results__list'),
+    searchRes: document.querySelector('.results')
+};
+
+export const elementStrings = {
+    loader: 'loader'
+};
+
+export const renderLoader = parent => {
+    //class="${elementStrings.loader}"
+    const loader = `
+        <div class="${elementStrings.loader}">
+            <svg>
+                <use href="img/icons.svg#icon-cw"></use>
+            </svg>
+        </div>`;
+    parent.insertAdjacentHTML('afterbegin', loader);
+};
+
+export const clearLoader = () => {
+    const loader = document.querySelector(`.${elementStrings.loader}`);
+
+    if(loader) loader.parentElement.removeChild(loader);
+}
